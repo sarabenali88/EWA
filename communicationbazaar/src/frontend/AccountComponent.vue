9223822b5f23b23432abe4c871ab5dc9d93dfe1e
Jasper Fernhout <jasper.trampe@hva.nl>
2023-09-26 15:42:04 +0200

    Profile

--- communicationbazaar/src/frontend/AccountComponent.vue
+++ communicationbazaar/src/frontend/AccountComponent.vue
@@ -1,11 +1,50 @@
 <template>
-  <h1>
-    account
-  </h1>
-
-  <button class="btn btn-primary">
-    Hallo
-  </button>
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-lg-6 mb-4 mb-lg-0">
+        <div class="card mb-3">
+          <div class="row g-0">
+            <div class="col-md-4 gradient-custom text-center text-white">
+              <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                   alt="Avatar" class="img-fluid my-5" id="profilePicture"/>
+            </div>
+            <div class="col-md-8">
+              <div class="card-body p-4">
+                <h6>Information</h6>
+                <hr class="mt-0 mb-4">
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>Name</h6>
+                    <p class="text-muted">Maria Howitz</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Email</h6>
+                    <p class="text-muted">info@example.com</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Role</h6>
+                    <p class="text-muted">Image maker</p>
+                  </div>
+                </div>
+                <h6>Projects</h6>
+                <hr class="mt-0 mb-4">
+                <div class="row pt-1">
+                  <div class="col-6 mb-3">
+                    <h6>On going</h6>
+                    <p class="text-muted">Lorem ipsum</p>
+                  </div>
+                  <div class="col-6 mb-3">
+                    <h6>Done</h6>
+                    <p class="text-muted">Dolor sit amet</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -16,4 +55,7 @@ export default {
 
 <style scoped>
 
+#profilePicture {
+  width: 80px;
+}
 </style>
