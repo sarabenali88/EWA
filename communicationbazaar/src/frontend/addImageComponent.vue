60a78356fe98adf301d5f9bb2d4893da0b37fbe7
benalis <sara.benali@hva.nl>
2023-10-01 15:17:51 +0200

    working on validation

--- communicationbazaar/src/frontend/addImageComponent.vue
+++ communicationbazaar/src/frontend/addImageComponent.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="container">
-    <form>
+    <Form>
       <div class="row">
         <div class="col-25">
           <label for="ean">EAN code</label>
         </div>
         <div class="col-75">
-          <input type="text" id="eanId" name="ean" placeholder="EAN code">
+          <div class="input-container">
+          <Field type="text" id="eanId" name="ean" placeholder="EAN code" :rules="[validateEan]" lazy/>
+          <ErrorMessage class="error" name="ean"/>
+          </div>
         </div>
       </div>
       <div class="row">
@@ -14,7 +17,11 @@
           <label for="start">Startklaar Versie</label>
         </div>
         <div class="col-75">
-          <input type="text" id="startId" name="start" placeholder="Startklaar versie">
+          <div class="input-container">
+          <Field type="text" id="startId" name="start" placeholder="Startklaar versie"
+                 :rules="[validateInput]" lazy/>
+            <ErrorMessage class="error" name="start"/>
+          </div>
         </div>
       </div>
       <div class="row">
@@ -22,7 +29,10 @@
           <label for="image">Image Naam</label>
         </div>
         <div class="col-75">
-          <input type="text" id="imageId" name="imageName" placeholder="Image naam">
+          <div class="input-container">
+          <Field type="text" id="imageId" name="image" placeholder="Image naam" :rules="[validateInput]" lazy/>
+            <ErrorMessage class="error" name="image"/>
+          </div>
         </div>
       </div>
       <div class="row">
@@ -30,7 +40,10 @@
           <label for="location">Image locatie</label>
         </div>
         <div class="col-75">
-          <input type="text" id="fname" name="imageLocation" placeholder="Image door winkel">
+          <div class="input-container">
+          <Field type="text" id="locationId" name="location" placeholder="Image door winkel" :rules="[validateInput]" lazy/>
+            <ErrorMessage class="error" name="location"/>
+            </div>
         </div>
       </div>
       <div class="row">
@@ -38,12 +51,14 @@
           <label for="status">Status</label>
         </div>
         <div class="col-75">
-          <select id="statusId" name="status">
-            <option value="">Kies een status</option>
+          <div class="input-container">
+          <Field as="select" id="statusId" name="status" :rules="[validateInput]" lazy>
             <option value="todo">Te doen</option>
             <option value="ongoing">Mee bezig</option>
             <option value="finished">Afgerond</option>
-          </select>
+          </Field>
+            <ErrorMessage class="error" name="status"/>
+          </div>
         </div>
       </div>
       <div class="row">
@@ -51,7 +66,10 @@
           <label for="date">Datum</label>
         </div>
         <div class="col-75">
-          <input type="date" id="dateId" name="date">
+          <div class="input-container">
+          <Field type="date" id="dateId" name="date" :rules="[validateInput]" lazy/>
+            <ErrorMessage class="error" name="date"/>
+          </div>
         </div>
       </div>
       <div class="row">
@@ -59,7 +77,10 @@
           <label for="week">Week</label>
         </div>
         <div class="col-75">
-          <input type="week" id="weekId" name="week">
+          <div class="input-container">
+          <Field type="week" id="weekId" name="week" :rules="[validateInput]" lazy/>
+            <ErrorMessage class="error" name="week"/>
+          </div>
         </div>
       </div>
       <div class="row">
@@ -67,28 +88,65 @@
           <label for="subject">Problemen</label>
         </div>
         <div class="col-75">
-          <input type="radio" value="yes" name="problem">Ja
-          <input type="radio" value="no" name="problem">Nee
-          <input type="radio" value="reported" name="problem">Gemeld bij SP
-        </div>
+          <Field type="radio" value="yes" name="problem" :rules="[validateInput]" lazy/>Ja
+          <Field type="radio" value="no" name="problem" :rules="[validateInput]" lazy/>Nee
+          <Field type="radio" value="reported" name="problem" :rules="[validateInput]" lazy/>Gemeld bij SP
+            <ErrorMessage class="error" name="problem"/>
+          </div>
       </div>
       <div class="row">
-        <input type="submit" value="Submit">
+        <button type="submit">
+          Opslaan
+        </button>
       </div>
-    </form>
+
+    </Form>
   </div>
 
 </template>
 
+
 <script>
+import { Form, Field, ErrorMessage } from 'vee-validate';
 export default {
   name: "addImageComponent",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  data() {
+    return {
+      eanCode: '',
+      startVersion: '',
+      imageName: '',
+      location: '',
+      selectedVModel: null,
+      imageListRoute: '/imageListRoute',
 
+    }
+  },
+  methods: {
+    validateEan(value) {
+        if (!value) {
+          return 'Dit veld is verplicht';
+        }
+        if (isNaN(value)) {
+          return "EAN code moet uit getallen bestaan";
+        }
+        return true;
+      },
+    validateInput(value){
+      if (!value) {
+        return 'Dit veld is verplicht';
+      }
+    },
+    }
 }
 </script>
 
 <style scoped>
-input[type=text], input[type=date], input[type=week], select, textarea{
+input[type=text], input[type=date], input[type=week], select, textarea {
   width: 70%;
   padding: 12px;
   border: 1px solid #ccc;
@@ -96,7 +154,8 @@ input[type=text], input[type=date], input[type=week], select, textarea{
   box-sizing: border-box;
   resize: vertical;
 }
-input[type=radio]{
+
+input[type=radio] {
   width: 5%;
   margin-top: 20px;
   border: 1px solid #ccc;
@@ -112,8 +171,8 @@ label {
 }
 
 /* Style the submit button */
-input[type=submit] {
-  background-color: #04AA6D;
+button[type=submit] {
+  background-color: #DA1C25;
   color: white;
   padding: 12px 10px;
   border: none;
@@ -150,6 +209,14 @@ input[type=submit] {
   display: table;
   clear: both;
 }
+.error{
+  color: darkred;
+}
+.input-container {
+  display: flex;
+  flex-direction: column;
+}
+
 
 /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
 @media screen and (max-width: 600px) {
