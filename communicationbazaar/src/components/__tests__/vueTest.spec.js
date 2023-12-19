bd897b144775b4c55d8e26d39df680f739c7e771
Jasper Fernhout <jasper.trampe@hva.nl>
2023-12-19 15:26:59 +0100

    test

--- communicationbazaar/src/components/__tests__/vueTest.spec.js
+++ communicationbazaar/src/components/__tests__/vueTest.spec.js
@@ -1,10 +1,36 @@
 import {mount} from "@vue/test-utils";
 import headerComponent from "@/components/HeaderComponent";
+import {ImagesAdaptor} from "@/services/ImagesAdaptor";
+import i18n from "@/i18n";
+// import nl from "@/locales/nl.json"
+// import en from "@/locales/en.json"
+// import fr from "@/locales/fr.json"
+
+describe('name', () => {
+// const translations = {
+//     en: en,
+//     fr: fr,
+//     nl: nl
+// };
+
+const mockI18n = {
+    t: (key, locale = 'nl') => {
+        return translations[locale][key];
+    }
+};
 
 let wrapper;
 
 beforeEach(function () {
-    wrapper = mount(headerComponent);
+    wrapper = mount(headerComponent, {
+        global: {
+            provide: {
+                imagesService: ImagesAdaptor, // Replace with your mocked service
+            }
+        }
+    });
+
+    wrapper.setData({$i18n: 'nl'})
 })
 
 it('should renders properly', function () {
@@ -14,4 +40,5 @@ it('should renders properly', function () {
     expect(wrapper.html(),
         `The header did not render the pageTitle`)
         .toContain(wrapper.vm.pageTitle);
+});
 });
