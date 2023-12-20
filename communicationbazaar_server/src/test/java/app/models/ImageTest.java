e100a523a731bf2c7974e86bf13ecf8b950930ba
benalis <sara.benali@hva.nl>
2023-12-20 13:48:37 +0100

    setup for tests

--- communicationbazaar_server/src/test/java/app/models/ImageTest.java
+++ communicationbazaar_server/src/test/java/app/models/ImageTest.java
@@ -2,12 +2,12 @@ package app.models;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 public class ImageTest {
 
     Image image1, image2;
-    Laptop laptop1, laptop2;
+    Laptop laptop1;
 
     @BeforeEach
     void setUp(){
@@ -18,9 +18,6 @@ public class ImageTest {
                 Image.Status.IMPOSSIBLE, Image.Release.NEW, Image.Problem.YES, 45, 2023,
                 "ImageNaam7", null);
 
-        this.laptop2 = new Laptop(1739468, 196548846290L, "HP", "VICTUS 15-FA0290ND",
-                "Intel Core i7-12700H", "16 GB", "1 TB", "GeForce RTX 3050 Ti",
-                "15.6 inch", "39.6 cm", "WIN11", 500);
         this.image2 = new Image(10010, laptop1, "MM V4.6", "Apeldoorn", "9-11-2023",
                 Image.Status.FINISHED, Image.Release.UPDATE, Image.Problem.YES, 45, 2023,
                 "ImageNaam7", null);
