bdc1efca659edbec4b689a40fc25082f443aeca6
Seyma Kaya <seyma.kaya2@hva.nl>
2023-12-20 14:05:49 +0100

    Test for Image model

--- communicationbazaar_server/src/test/java/app/models/ImageTest.java
+++ communicationbazaar_server/src/test/java/app/models/ImageTest.java
@@ -1,4 +1,22 @@
 package app.models;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ImageTest {
+
+    Image image1, image1a;
+
+    @BeforeEach
+    void setup(){
+       this.image1 = Image.createSampleImage(1001);
+       this.image1a = Image.createSampleImage(1001);
+    }
+
+    @Test
+    void createSampleImageCreatesTwoDifferentImages(){
+        assertNotSame(this.image1.hashCode(), this.image1a.hashCode());
+    }
 }
