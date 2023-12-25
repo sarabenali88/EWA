c259283453a37d17e9fcf38912c167ee799a7148
Seyma Kaya <seyma.kaya2@hva.nl>
2023-12-25 17:18:22 +0100

    Test for Image repo and rest

---
+++ communicationbazaar_server/src/test/java/app/repositories/ImageRepositoryTest.java
@@ -0,0 +1,18 @@
+package app.repositories;
+
+import app.models.Image;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ImageRepositoryTest {
+
+    @Autowired
+    private Repository<Image> imagesRepo;
+
+
+
+}
---
+++ communicationbazaar_server/src/test/java/app/rest/ImageControllerTest.java
@@ -0,0 +1,17 @@
+package app.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ImageControllerTest {
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+}
