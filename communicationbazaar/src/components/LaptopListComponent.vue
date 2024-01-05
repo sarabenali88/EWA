06965df9275a18095ac2808b0c37572f2cbfd918
Seyma Kaya <seyma.kaya2@hva.nl>
2024-01-05 12:38:15 +0100

    Laptop can be imported

--- communicationbazaar/src/components/LaptopListComponent.vue
+++ communicationbazaar/src/components/LaptopListComponent.vue
@@ -335,8 +335,8 @@ export default {
           //creates laptop object and adds it to database
           const newLaptop = new Laptop(laptop[0], laptop[1], laptop[2], laptop[3], laptop[4], laptop[5], laptop[6], laptop[7], laptop[8], laptop[9], laptop[10], laptop[11])
           if (newLaptop.ean !== "ean") {
-            await this.laptopsService.asyncSave(newLaptop);
-            console.log(this.laptopsService.asyncSave(newLaptop))
+            const lap = await this.laptopsService.asyncSave(newLaptop);
+            console.log(lap)
           }
         }
       }
