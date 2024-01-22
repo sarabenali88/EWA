92284ce7df02caa7f0632ae2b79312612247c2fa
jonathanfricardo <jonathan.ricardo@hva.nl>
2024-01-22 11:20:12 +0100

    Added comments

--- communicationbazaar/src/components/__tests__/accountTest.spec.js
+++ communicationbazaar/src/components/__tests__/accountTest.spec.js
@@ -3,106 +3,114 @@ import ProfilePageComponent from "@/components/ProfilePageComponent";
 import i18n from "@/i18n";
 import {reactive} from "vue";
 
-
+/**
+ * A test class for our relation between Image and Account
+ *
+ * @author Jonathan
+ */
 describe("ProfilePageComponent", () => {
 
+    //variable to store the component wrapper
     let wrapper;
 
+    //mock data for testing
+    const mockAccounts = [
+        {
+            personalNumber: 10000,
+            password: "IloveApples4%",
+            name: "Pieter van den Broek",
+            email: "pietervdbroek@gmail.com",
+            role: "admin",
+            location: "Amsterdam",
+            loggedIn: true
+        },
+        {
+            personalNumber: 10001,
+            password: "IloveApps4%",
+            name: "Piet van den Broek",
+            email: "pietervdbroek@gmail.com",
+            role: "admin",
+            location: "Amsterdam",
+            loggedIn: false
+        }
+    ]
+
+    const mockImages = [
+        {
+            id: 1,
+            laptop: {
+                ean: 121412301239
+            },
+            version: 'Sample Version',
+            store: 'Sample Store',
+            upDateDate: "24-7-2023", // Sample date, you can adjust this as needed
+            status: "TODO", // Using the predefined status 'Te doen'
+            release: "NEW", // Using the predefined release 'Nieuw'
+            problem: "YES", // Using the predefined problem 'Ja'
+            createdWeek: 1, // Sample createdWeek value
+            createdYear: 2023, // Sample createdYear value
+            name: 'Sample Name 1',
+            comment: 'Sample Comment',
+            imageMaker: {
+                name: "Pieter van den Broek"
+            }
+        },
+        {
+            id: 2,
+            laptop: {
+                ean: 1266630139
+            },
+            version: 'Sample Version 2',
+            store: 'Sample Store',
+            upDateDate: "27-7-2023", // Sample date, you can adjust this as needed
+            status: "TODO", // Using the predefined status 'Te doen'
+            release: "NEW", // Using the predefined release 'Nieuw'
+            problem: "YES", // Using the predefined problem 'Ja'
+            createdWeek: 1, // Sample createdWeek value
+            createdYear: 2023, // Sample createdYear value
+            name: 'Sample Name 2',
+            comment: 'Sample Comment',
+            imageMaker: {
+                name: "Pieter van den Broek"
+            }
+        },
+    ];
+
+    const mockLaptops = [
+        {
+            articleNumber: 12312412,
+            ean: 81812838,
+            brand: "LENOVO",
+            description: "test description",
+            processor: "test cpu",
+            ram: "test",
+            storage: "big",
+            gpu: "fast",
+            sizeInch: "massive",
+            sizeCm: "massive",
+            os: "linux",
+            prize: 100
+        },
+        {
+            articleNumber: 12312222412,
+            ean: 81123652838,
+            brand: "ACER",
+            description: "test description",
+            processor: "test cpu",
+            ram: "test",
+            storage: "tiny",
+            gpu: "Slow",
+            sizeInch: "small",
+            sizeCm: "small",
+            os: "linux",
+            prize: 100
+        }
+    ]
+
+    //setting up mock services for dependency injection
     beforeEach(async () => {
 
-        const mockAccounts = [
-            {
-                personalNumber: 10000,
-                password: "IloveApples4%",
-                name: "Pieter van den Broek",
-                email: "pietervdbroek@gmail.com",
-                role: "admin",
-                location: "Amsterdam",
-                loggedIn: true
-            },
-            {
-                personalNumber: 10001,
-                password: "IloveApps4%",
-                name: "Piet van den Broek",
-                email: "pietervdbroek@gmail.com",
-                role: "admin",
-                location: "Amsterdam",
-                loggedIn: false
-            }
-        ]
-
-        const mockImages = [
-            {
-                id: 1,
-                laptop: {
-                    ean: 121412301239
-                },
-                version: 'Sample Version',
-                store: 'Sample Store',
-                upDateDate: "24-7-2023", // Sample date, you can adjust this as needed
-                status: "TODO", // Using the predefined status 'Te doen'
-                release: "NEW", // Using the predefined release 'Nieuw'
-                problem: "YES", // Using the predefined problem 'Ja'
-                createdWeek: 1, // Sample createdWeek value
-                createdYear: 2023, // Sample createdYear value
-                name: 'Sample Name 1',
-                comment: 'Sample Comment',
-                imageMaker: {
-                    name: "Pieter van den Broek"
-                }
-            },
-            {
-                id: 2,
-                laptop: {
-                    ean: 1266630139
-                },
-                version: 'Sample Version 2',
-                store: 'Sample Store',
-                upDateDate: "27-7-2023", // Sample date, you can adjust this as needed
-                status: "TODO", // Using the predefined status 'Te doen'
-                release: "NEW", // Using the predefined release 'Nieuw'
-                problem: "YES", // Using the predefined problem 'Ja'
-                createdWeek: 1, // Sample createdWeek value
-                createdYear: 2023, // Sample createdYear value
-                name: 'Sample Name 2',
-                comment: 'Sample Comment',
-                imageMaker: {
-                    name: "Pieter van den Broek"
-                }
-            },
-        ];
-
-        const mockLaptops = [
-            {
-                articleNumber: 12312412,
-                ean: 81812838,
-                brand: "LENOVO",
-                description: "test description",
-                processor: "test cpu",
-                ram: "test",
-                storage: "big",
-                gpu: "fast",
-                sizeInch: "massive",
-                sizeCm: "massive",
-                os: "linux",
-                prize: 100
-            },
-            {
-                articleNumber: 12312222412,
-                ean: 81123652838,
-                brand: "ACER",
-                description: "test description",
-                processor: "test cpu",
-                ram: "test",
-                storage: "tiny",
-                gpu: "Slow",
-                sizeInch: "small",
-                sizeCm: "small",
-                os: "linux",
-                prize: 100
-            }
-        ]
-
+        //mock services for dependecy injection
         const imagesService = {
             asyncFindAll: jest.fn().mockResolvedValue(mockImages),
         };
@@ -116,6 +124,7 @@ describe("ProfilePageComponent", () => {
             asyncGetImagesFromAccount : jest.fn().mockResolvedValue(mockImages)
         }
 
+        //mounting the component with mocked services and initial data
         wrapper = mount(ProfilePageComponent, {
             global: {
                 provide: {
@@ -137,10 +146,12 @@ describe("ProfilePageComponent", () => {
 
     it("should display correct amount of images on profile page", async () => {
 
+        // Get the actual images from the component
         const images = wrapper.vm.images
-        expect(images).toHaveLength(2) // Get the actual images from the component
+        expect(images).toHaveLength(2)
 
-        const expectedRowCount = images.length * 2; // Each image is represented by two rows
+        // Each image is represented by two rows
+        const expectedRowCount = images.length * 2;
         const tableRows = wrapper.findAll("tbody tr");
         expect(tableRows).toHaveLength(expectedRowCount);
 
@@ -148,9 +159,12 @@ describe("ProfilePageComponent", () => {
 
 
     it("should display correct image names in the table", () => {
+
+        // Get all images and table rows from profile
         const images = wrapper.vm.images;
         const tableRows = wrapper.findAll("tbody tr");
 
+        // For each row, check if the imageMaker name is correct
         tableRows.forEach((row, index) => {
             const expectedName = images[Math.floor(index / 2)].imageMaker.name;
             expect(row.text()).toContain(expectedName);
@@ -158,6 +172,8 @@ describe("ProfilePageComponent", () => {
     });
 
     it("should delete the selected image when calling deleteImage", async () => {
+
+        // Select the first image and delete it
         const selectedImage = wrapper.vm.images[0];
         wrapper.setData({ selectedImage });
         await wrapper.vm.deleteImage();
--- communicationbazaar/src/components/__tests__/searchTest.spec.js
+++ communicationbazaar/src/components/__tests__/searchTest.spec.js
@@ -3,83 +3,91 @@ import headerComponent from "@/components/HeaderComponent";
 import i18n from "@/i18n";
 import {reactive} from "vue";
 
-
+/**
+ * A test class for our header search class
+ *
+ * @author Jonathan
+ */
 describe("HeaderComponent", () => {
 
+    //variable to store the component wrapper
     let wrapper;
 
-    beforeEach(async () => {
-        const mockImages = [
-            {
-                id: 1,
-                laptop: {
-                    ean: 121412301239
-                },
-                version: 'Sample Version',
-                store: 'Sample Store',
-                upDateDate: "24-7-2023", // Sample date, you can adjust this as needed
-                status: "TODO", // Using the predefined status 'Te doen'
-                release: "NEW", // Using the predefined release 'Nieuw'
-                problem: "YES", // Using the predefined problem 'Ja'
-                createdWeek: 1, // Sample createdWeek value
-                createdYear: 2023, // Sample createdYear value
-                name: 'Sample Name 1',
-                comment: 'Sample Comment',
-                imageMaker: {
-                    name: "Test"
-                }
+    //mock data for testing
+    const mockImages = [
+        {
+            id: 1,
+            laptop: {
+                ean: 121412301239
             },
-            {
-                id: 2,
-                laptop: {
-                    ean: 1266630139
-                },
-                version: 'Sample Version 2',
-                store: 'Sample Store',
-                upDateDate: "27-7-2023", // Sample date, you can adjust this as needed
-                status: "TODO", // Using the predefined status 'Te doen'
-                release: "NEW", // Using the predefined release 'Nieuw'
-                problem: "YES", // Using the predefined problem 'Ja'
-                createdWeek: 1, // Sample createdWeek value
-                createdYear: 2023, // Sample createdYear value
-                name: 'Sample Name 2',
-                comment: 'Sample Comment',
-                imageMaker: {
-                    name: "Test"
-                }
-            },
-        ];
-
-        const mockLaptops = [
-            {
-                articleNumber: 12312412,
-                ean: 81812838,
-                brand: "LENOVO",
-                description: "test description",
-                processor: "test cpu",
-                ram: "test",
-                storage: "big",
-                gpu: "fast",
-                sizeInch: "massive",
-                sizeCm: "massive",
-                os: "linux",
-                prize: 100
-            },
-            {
-                articleNumber: 12312222412,
-                ean: 81123652838,
-                brand: "ACER",
-                description: "test description",
-                processor: "test cpu",
-                ram: "test",
-                storage: "tiny",
-                gpu: "Slow",
-                sizeInch: "small",
-                sizeCm: "small",
-                os: "linux",
-                prize: 100
+            version: 'Sample Version',
+            store: 'Sample Store',
+            upDateDate: "24-7-2023", // Sample date, you can adjust this as needed
+            status: "TODO", // Using the predefined status 'Te doen'
+            release: "NEW", // Using the predefined release 'Nieuw'
+            problem: "YES", // Using the predefined problem 'Ja'
+            createdWeek: 1, // Sample createdWeek value
+            createdYear: 2023, // Sample createdYear value
+            name: 'Sample Name 1',
+            comment: 'Sample Comment',
+            imageMaker: {
+                name: "Test"
             }
-        ]
+        },
+        {
+            id: 2,
+            laptop: {
+                ean: 1266630139
+            },
+            version: 'Sample Version 2',
+            store: 'Sample Store',
+            upDateDate: "27-7-2023", // Sample date, you can adjust this as needed
+            status: "TODO", // Using the predefined status 'Te doen'
+            release: "NEW", // Using the predefined release 'Nieuw'
+            problem: "YES", // Using the predefined problem 'Ja'
+            createdWeek: 1, // Sample createdWeek value
+            createdYear: 2023, // Sample createdYear value
+            name: 'Sample Name 2',
+            comment: 'Sample Comment',
+            imageMaker: {
+                name: "Test"
+            }
+        },
+    ];
+
+    const mockLaptops = [
+        {
+            articleNumber: 12312412,
+            ean: 81812838,
+            brand: "LENOVO",
+            description: "test description",
+            processor: "test cpu",
+            ram: "test",
+            storage: "big",
+            gpu: "fast",
+            sizeInch: "massive",
+            sizeCm: "massive",
+            os: "linux",
+            prize: 100
+        },
+        {
+            articleNumber: 12312222412,
+            ean: 81123652838,
+            brand: "ACER",
+            description: "test description",
+            processor: "test cpu",
+            ram: "test",
+            storage: "tiny",
+            gpu: "Slow",
+            sizeInch: "small",
+            sizeCm: "small",
+            os: "linux",
+            prize: 100
+        }
+    ]
+
+
+    beforeEach(async () => {
 
         const imagesService = {
             asyncFindAll: jest.fn().mockResolvedValue(mockImages),
